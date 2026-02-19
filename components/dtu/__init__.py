dbf6414fc7307ad5f3823cc91c613bdff9c1335a
dentra <dennis.trachuk@gmail.com>
2026-02-19 20:33:47 +0300

    dtu: support esphome 2026.2.0 and up

--- components/dtu/__init__.py
+++ components/dtu/__init__.py
@@ -1,1 +1,14 @@
+import esphome.config_validation as cv
+from esphome.const import (
+    __version__ as ESPHOME_VERSION,
+)
+
+if cv.Version.parse(ESPHOME_VERSION) > cv.Version.parse("2026.1.99"):
+    from esphome.components.esp32 import include_builtin_idf_component
+
 CODEOWNERS = ["@dentra"]
+
+
+async def to_code(config):
+    if cv.Version.parse(ESPHOME_VERSION) > cv.Version.parse("2026.1.99"):
+        include_builtin_idf_component("esp_http_client")
